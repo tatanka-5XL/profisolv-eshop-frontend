5cee400d26a3af3684520593982b467d4430b0d7
petrpribil <petr.pribil@profisolv.com>
2023-04-08 17:52:19 +0200

    Modified main page with company details

--- pages/index_original.js
+++ pages/index_original.js
@@ -5,41 +5,59 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Profisolv - hlavní stránka</title>
+        <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <img src='/logo_profisolv_print_cisteni_2023.png'></img>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+
         <p className={styles.description}>
-          Zde pro Vás tvořím své nové stránky s eshopem
+          Get started by editing <code>pages/index.js</code>
         </p>
 
         <div className={styles.grid}>
-          <div className={styles.card}>
-            <h3>Eshop &rarr;</h3>
-            <p>Budete si moci prohlédnout a objednat produkty</p>
-          </div>
+          <a href="https://nextjs.org/docs" className={styles.card}>
+            <h3>Documentation &rarr;</h3>
+            <p>Find in-depth information about Next.js features and API.</p>
+          </a>
 
-          <div className={styles.card}>
-            <h3>Technické info &rarr;</h3>
-            <p>Poradíme, který produkt vybrat pro jakou aplikaci</p>
-          </div>
+          <a href="https://nextjs.org/learn" className={styles.card}>
+            <h3>Learn &rarr;</h3>
+            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          </a>
 
-          <div className={styles.card}>
-            <h3>Naši dodavatelé &rarr;</h3>
-            <p>Stručné info o našich prověřených partnerech</p>
-          </div>
+          <a
+            href="https://github.com/vercel/next.js/tree/master/examples"
+            className={styles.card}
+          >
+            <h3>Examples &rarr;</h3>
+            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          </a>
 
-          <div className={styles.card}>
-            <h3>Informace o firmě &rarr;</h3>
-            <p>A také samozřejmě nějaké to info o nás.</p>
-          </div>
+          <a
+            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            className={styles.card}
+          >
+            <h3>Deploy &rarr;</h3>
+            <p>
+              Instantly deploy your Next.js site to a public URL with Vercel.
+            </p>
+          </a>
         </div>
       </main>
 
       <footer>
-          Vytvořil Profisolv&copy; 2023
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
+        </a>
       </footer>
 
       <style jsx>{`
